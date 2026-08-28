956a169cb717cc8dcadd69cfd246c1d43de1f0e7
Petter Arvidsson <petter.arvidsson@gmail.com>
2026-08-28 10:58:38 +0200

    Align IOs better

--- src/pio-display.c
+++ src/pio-display.c
@@ -4,11 +4,11 @@
 #include "hardware/dma.h"
 #include "spi.pio.h"
 
-#define RESET 21
-#define SCLK 18
-#define MOSI 19
+#define RESET 19
+#define SCLK 16
+#define MOSI 6
 #define CS 17
-#define DC 22
+#define DC 18
 
 static uint dma_init(PIO pio, uint sm) {
   int channel = dma_claim_unused_channel(true);
@@ -28,9 +28,6 @@ static uint dma_init(PIO pio, uint sm) {
 #define BIT(D, N) (((uint8_t)(D) >> (N)) & 0x1)
 #define BIT2(D, N) (BIT(D, N) << ((N) * 2)) | (BIT(D, N) << ((N) * 2 + 1))
 #define BS(B) (BIT2(((B >> 4) & 0xF), 0) | BIT2(((B >> 4) & 0xF), 1) | BIT2(((B >> 4) & 0xF), 2) | BIT2(((B >> 4) & 0xF), 3)), (BIT2(B, 0) | BIT2(B, 1) | BIT2(B, 2) | BIT2(B, 3))
-
-#define INT16_SPREAD(I) (((uint8_t)BYTE2(((I) >> 8)) << 16) | BYTE2(I))
-#define UINT32_SPREAD(U) INT16_SPREAD((U) >> 16),  INT16_SPREAD(U)
 
 #define PATTERN BS(0x01), BS(0x00), BS(0x00), BS(0x00)
 #define WORDS 32
@@ -87,39 +84,6 @@ static uint8_t data[] = {
   PATTERN, PATTERN, PATTERN, PATTERN ,PATTERN, PATTERN, PATTERN, PATTERN
 };
 
-void print_buf() {
-  uint8_t *fb = data + 3 * 4;
-  for(uint32_t i = 0; i < sizeof(data); i++) {
-    if(i % 4 == 0)
-      printf("|");
-    printf("%02x", fb[i]);
-  }
-  printf("\n");
-}
-
-void compare_byte_bits() {
-  size_t size = 36; //sizeof(data);
-  uint8_t *udata = (uint8_t*)data;
-
-  for(size_t i = 0; i < size; i++) {
-    uint8_t b = udata[i];
-    uint8_t b0 = b & 0x1;
-    uint8_t b1 = (b >> 1) & 0x1;
-    uint8_t b2 = (b >> 2) & 0x1;
-    uint8_t b3 = (b >> 3) & 0x1;
-    uint8_t b4 = (b >> 4) & 0x1;
-    uint8_t b5 = (b >> 5) & 0x1;
-    uint8_t b6 = (b >> 6) & 0x1;
-    uint8_t b7 = (b >> 7) & 0x1;
-
-    if(b0 != b1 || b2 != b3 || b4 != b5 || b6 != b7) {
-      printf("M[%d]: %2x\n", i, b);
-    } else {
-      printf("A[%d]: %2x\n", i, b);
-    }
-  }
-}
-
 #define DISPLAYS 2
 #define DISPLAY_ROW (32 * 4 * DISPLAYS)
 #define DISPLAY_ROW_HEADER (8 + 4 * DISPLAYS)
@@ -151,9 +115,7 @@ void pixel(const uint8_t display, const uint8_t x, const uint8_t y, const bool o
 
 int main() {
     stdio_init_all();
-    print_buf();
     pixel(0, 0, 0, 1);
-    print_buf();
 
     pixel(0, 0, 1, 1);
     pixel(0, 0, 2, 1);
