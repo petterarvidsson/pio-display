358597f8e9098906d576be16b26761351b9ad9b1
Petter Arvidsson <petter.arvidsson@gmail.com>
2026-08-30 12:31:52 +0200

    Add more tests

--- tests/fb_ops_test.c
+++ tests/fb_ops_test.c
@@ -21,8 +21,33 @@ void test_split_bytes_into() {
   split_bytes_into(t2, 2, buf, 3);
   test_array(r2, 6);
 }
+
+void test_split_bytes_into_ff() {
+  uint8_t t2[] = { 0xFF, 0xFF };
+  uint8_t r2[] = { 0xFF,0xFF,0xFF,0xFF,0xFF,0xFF };
+  split_bytes_into(t2, 2, buf, 3);
+  test_array(r2, 6);
+}
+
+void test_split_bytes_into_00() {
+  uint8_t t2[] = { 0x00, 0x00 };
+  uint8_t r2[] = { 0x00,0x00,0x00,0x00,0x00,0x00 };
+  split_bytes_into(t2, 2, buf, 3);
+  test_array(r2, 6);
+}
+
+void test_split_bytes_into_4bytes() {
+  uint8_t t2[] = { 0x03, 0x8C };
+  uint8_t r2[] = { 0x00,0x00,0x00,0xFF,0xF0,0x00,0xFF,0x00 };
+  split_bytes_into(t2, 2, buf, 4);
+  test_array(r2, 8);
+}
+
 TEST_LIST = {
   { "split_byte_into", test_split_byte_into },
   { "split_bytes_into", test_split_bytes_into },
+  { "split_bytes_into FF", test_split_bytes_into_ff },
+  { "split_bytes_into 00", test_split_bytes_into_00 },
+  { "split_bytes_into 4 bytes", test_split_bytes_into_4bytes },
   { NULL, NULL }
 };
